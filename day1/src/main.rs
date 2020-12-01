bfcf636097fea4bd29866100598461e8ed19b9d7
Axel Schlueter <axel@pqrs.de>
2020-12-01 23:01:38 +0100

    simple solution for the first day

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,15 +3,37 @@
 //
 
 use std::fs::File;
-use std::io;
-// use std::io::{self, BufRead};
-// use std::path::Path;
+use std::io::{self, BufRead};
 
 fn main() {
-  println!("Hello, world!");
+  let file = File::open("input.txt")
+    .expect("This should have been a file?!");
 
-  // let file = File::open("input.txt")?;
-  let file = File::open("input.txt");
+  let mut numbers = Vec::<i32>::new();
+  for line in io::BufReader::new(file).lines() {
+    if let Ok(line) = line {
+      numbers.push(line.parse::<i32>().unwrap());
+    }
+  }
+  numbers.sort();
 
-  let lines = Ok(io::BufReader::new(file).lines());
+  while let Some(large) = numbers.pop() {
+    for outer in 1..numbers.len() {
+      let middle = numbers[numbers.len() - outer];
+      for inner in 1..outer {
+        let small = numbers[numbers.len() - inner];
+        if large + middle + small == 2020 {
+          println!("we've found it: {} + {} + {} = 2020, solution is {}",
+          large, middle, small, large * middle * small);
+        }
+      }
+    }
+
+    // for i in 1..numbers.len() {
+    //   let small = numbers[numbers.len() - i];
+    //   if large + small == 2020 {
+    //     println!("we've found it: {} + {} = 2020, solution is {}", large, small, large * small);
+    //   }
+    // }
+  }
 }
