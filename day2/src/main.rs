b73e173eeebe2b413e0c09253fe796ea9f4e2f18
Axel Schlueter <axel@pqrs.de>
2020-12-05 22:36:26 +0100

    day 2 solved for both parts

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,19 +7,25 @@ use std::io::{self, BufRead};
 use std::collections::HashMap;
 use regex::Regex;
 
-fn validate_line(line: &str, min: i32, max: i32, what: char) -> bool {
-  println!("xxxm {} to {}, for {} in {}", min, max, what, line);
-
+// return true if the number of `what` characters in the
+// `line' is within the range of min...max
+fn validate_line_part1(line: &str, min: i32, max: i32, what: char) -> bool {
   let mut map = HashMap::new();
   for c in line.chars() {
     *(map.entry(c).or_insert(0)) += 1;
-    println!("{}", c)
   }
   let count: &i32 = map.entry(what).or_insert(-1);
-  println!("count is {}", count);
   return count >= &min && count <= &max
 }
 
+// return true if the `what` character is at either
+// `pos1' or `pos2` in the `line`
+fn validate_line_part2(line: &str, pos1: usize, pos2: usize, what: char) -> bool {
+  let chars: Vec<char> = line.chars().collect();
+  let found1 = chars[pos1 - 1] == what;
+  let found2 = chars[pos2 - 1] == what;
+  return (found1 && !found2) || (!found1 && found2)
+}
 
 fn main() {
   let file = File::open("input.txt")
@@ -27,19 +33,26 @@ fn main() {
 
   let re = Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
 
+  let mut num_passwords = 0;
+  let mut valid_passwords = 0;
   for line in io::BufReader::new(file).lines() {
+    num_passwords += 1;
     if let Ok(line) = line {
       let cap = re.captures_iter(&line).next();
       if let Some(m) = &cap {
-        validate_line(&m[4],
+        if validate_line_part2(&m[4],
           m[1].parse().unwrap(),
           m[2].parse().unwrap(),
-          m[3].parse().unwrap());
-return;
+          m[3].parse().unwrap())
+        {
+          valid_passwords += 1;
+        }
       } else  {
         println!("invalid list <{}>!?", line);
         return;
       }
     }
   }
+  println!("we found {} valid password, out of {} (invalid: {}).",
+    valid_passwords, num_passwords, num_passwords - valid_passwords);
 }
